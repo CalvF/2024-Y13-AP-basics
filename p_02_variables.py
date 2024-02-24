b4524b84a943669e579b23b8d7c3859dc07387ae
CalvF <110645028+CalvF@users.noreply.github.com>
2024-02-24 21:15:22 +1300

    Variable basics

---
+++ p_02_variables.py
@@ -0,0 +1,15 @@
+#Code to learn about variable...
+
+#Variable can be a string (note the speack marks)
+greeting = "hello world"
+
+#Variable can be a number
+fav_num = 7
+
+# Variable can be Boolean (True/ False)
+love_programming = False
+
+print(greeting)
+print(fav_num)
+print(love_programming)
+
